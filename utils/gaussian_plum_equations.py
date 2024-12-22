c29acb24353f2597401d14fe0431ce71474a689b
tbf <contact@trailblazingfive.net>
2024-12-22 23:47:27 +0100

    fix nan error

--- utils/gaussian_plum_equations.py
+++ utils/gaussian_plum_equations.py
@@ -5,25 +5,29 @@ ROUND_VALUE = True
 ROUNDING_DIGITS = 4
 
 def sigma_y(x, stability_class):
+    if x <= 0:
+        return np.nan
     a1 = stability_classes[stability_class]["a1"]
     a2 = stability_classes[stability_class]["a2"]
-    sigma = np.float64((a1 * np.log(x)+ a2)*x) 
+    sigma = np.float64((a1 * np.log(x) + a2) * x) 
     if ROUND_VALUE:
         return np.round(sigma, ROUNDING_DIGITS)
     else:
         return sigma
 
 def sigma_z(x, stability_class):
+    if x <= 0:
+        return np.nan
     b1 = stability_classes[stability_class]["b1"]
     b2 = stability_classes[stability_class]["b2"]
     b3 = stability_classes[stability_class]["b3"]
-    sigma = np.float64((1.0/2.15)*np.exp(b1+b2*np.log(x)+b3*np.log(x)**2 ))
+    sigma = np.float64((1.0/2.15) * np.exp(b1 + b2 * np.log(x) + b3 * np.log(x)**2))
     if ROUND_VALUE:
         return np.round(sigma, ROUNDING_DIGITS)
     else:
         return sigma
 
-for i in np.arange(0,10,0.1):
+for i in np.arange(0.1, 10, 0.1):  # Start at 0.1 to avoid log(0)
     sigma_y_value = sigma_y(i, "A")
     sigma_z_value = sigma_z(i, "A")
     print(f"sy={sigma_y_value}, sz={sigma_z_value}")
